700635348a1b823f1f927115f2bc4b9bff41547b
hamsik22 <payback9488@gmail.com>
2023-09-11 00:24:36 +0900

    ➕[Add]: Create TimerView.swift

---
+++ FillTime/TimerView.swift
@@ -0,0 +1,20 @@
+//
+//  TimerView.swift
+//  FillTime
+//
+//  Created by 황석현 on 2023/09/11.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TimerView_Previews: PreviewProvider {
+    static var previews: some View {
+        TimerView()
+    }
+}
