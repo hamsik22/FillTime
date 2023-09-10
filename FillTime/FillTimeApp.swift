e0df7ae3548e30c623573c835534be00023580a8
hamsik22 <payback9488@gmail.com>
2023-09-10 23:45:48 +0900

    🎉[Init]: First Commit

---
+++ FillTime/FillTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FillTimeApp.swift
+//  FillTime
+//
+//  Created by 황석현 on 2023/09/10.
+//
+
+import SwiftUI
+
+@main
+struct FillTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
