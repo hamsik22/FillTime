c443b9e14bdc4fdb32a9863936e33be6ef5ea1e8
Hamsik22 <payback9488@gmail.com>
2024-01-13 15:12:37 +0900

    [+]: Add Code
    - Add scenePhase

--- FillTime/FillTimeApp.swift
+++ FillTime/FillTimeApp.swift
@@ -9,9 +9,25 @@ import SwiftUI
 
 @main
 struct FillTimeApp: App {
+    
+    @Environment(\.scenePhase) private var scenePhase
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        .onChange(of: scenePhase) { phase in
+            switch phase {
+                
+            case .background:
+                print("App Entered Background!")
+            case .inactive:
+                print("App Entered Inactive!")
+            case .active:
+                print("App Entered Active!")
+            @unknown default:
+                print("App Entered Unknown Phase!")
+            }
         }
     }
 }
