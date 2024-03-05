27f05755999d0f5adacc60679bdcbda80bb10e01
HwangSukHyeon <payback9488@gmail.com>
2024-03-05 14:48:32 +0900

    [UI]: 시간 선택 초기화면
    - Picker를 활용하여 기초 화면 구현

---
+++ FillTime/View/Component/TimeSelector.swift
@@ -0,0 +1,47 @@
+//
+//  TimeSelector.swift
+//  FillTime
+//
+//  Created by 황석현 on 3/4/24.
+//
+
+import SwiftUI
+
+struct TimeSelector: View {
+    
+    @State var selections: (Int, Int) = (0, 0)
+    var hours: [Int] = Array(0...24)
+    var minute: [Int] = Array(0...59)
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Picker("Picker", selection: $selections.0) {
+                    ForEach(hours, id: \.self) { item in
+                        Text("\(hours[item])")
+                            .tag("\(item)")
+                    }
+                }
+                .padding(.leading)
+                
+                Text("시간")
+                    
+                Picker("Picker", selection: $selections.1) {
+                    ForEach(minute, id: \.self) { item in
+                        Text("\(minute[item])")
+                            .tag("\(item)")
+                    }
+                }
+                
+                Text("분")
+                    .padding(.trailing)
+            }
+            .pickerStyle(.wheel)
+            Text("\(selections.0) 시간 : \(selections.1) 분")
+        }
+    }
+}
+
+#Preview {
+    TimeSelector()
+}
