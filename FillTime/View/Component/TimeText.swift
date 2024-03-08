597422f16d379bcd7b2a77ed9adad8a70c897046
HwangSukHyeon <payback9488@gmail.com>
2024-03-08 16:59:27 +0900

    [File&Design]: Time GaugeBar, Text
    - 파일 2개 추가(TimeGaugeBar/TimeText)
    - 남은 시간을 표시하는 게이지 바 UI 구현

---
+++ FillTime/View/Component/TimeText.swift
@@ -0,0 +1,19 @@
+//
+//  TimeText.swift
+//  FillTime
+//
+//  Created by 황석현 on 3/5/24.
+//
+
+import SwiftUI
+
+struct TimeText: View {
+    var body: some View {
+        Text("Hello, World!")
+            .font(.largeTitle)
+    }
+}
+
+#Preview {
+    TimeText()
+}
