d8114afdbf86ac74136d410bb51620456b0cb373
HwangSukHyeon <payback9488@gmail.com>
2024-04-16 17:31:10 +0900

    [View]: 간단한 뷰 테스트

--- FillTime/View/MockTimeSettingView.swift
+++ FillTime/View/MockTimeSettingView.swift
@@ -8,11 +8,29 @@
 import SwiftUI
 
 struct MockTimeSettingView: View {
+    
+    var hourArray = Array(stride(from: 1, to: 24, by: 5))
+    var minuteArray = Array(stride(from: 1, to: 60, by: 5))
+    @ObservedObject var viewModel = MockClass()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+                Picker("시간을 정하세요", selection: $viewModel.hourData) {
+                    ForEach(hourArray, id: \.self) { item in
+                        Text("\(item)").tag(item)
+                    }
+                }.pickerStyle(.wheel)
+            Text("HourData = \(viewModel.hourData)")
+            Text("HourData = \(viewModel.minuteData)")
+        }
     }
 }
 
 #Preview {
     MockTimeSettingView()
+}
+
+class MockClass: ObservableObject {
+    @Published var hourData = 0
+    @Published var minuteData = 59
 }
