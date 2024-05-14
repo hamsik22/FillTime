ec3965e1b5d652707e4e97bccaa8dfb3ebe1f8c2
HwangSukHyeon <payback9488@gmail.com>
2024-05-14 19:29:53 +0900

    [Code&Func]반복 타이머 기능 구현
    - Model의 데이터로 반복 타이머 함수 구현
    - 문서 수정
    - 테스트 함수 1개 삭제

--- FillTime/Model/TimeModel.swift
+++ FillTime/Model/TimeModel.swift
@@ -10,10 +10,14 @@ import Foundation
 class TimeModel {
     
     var timer = Timer()
+    var workTime = 5
+    var restTime = 5
+    var cycle = 2
+    var timerState = true
     
-    // TODO: 가장 기본적인 타이머 코드를 만들자
-    func startTimer(for time: Int) {
-        var timeLeft = time
+    // MARK: 가장 기본적인 타이머 코드
+    func startTimer() {
+        var timeLeft = 5
         timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { t in
             if timeLeft == 0 {
                 t.invalidate()
@@ -23,5 +27,35 @@ class TimeModel {
                 print("timeLeft : \(timeLeft)")
             }
         }
+    }
+    
+    // MARK: 타이머 반복 실행
+    func startLoopTimer() {
+        var timeLeft = 0
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true, block: { [self] t in
+            if timeLeft == 0 {
+                if cycle > 0 {
+                    if timerState {
+                        timeLeft = workTime
+                        print("TimeLeft = WorkTime")
+                        timerState.toggle()
+                        print("TimeState = \(timerState)")
+                    } else {
+                        timeLeft = restTime
+                        print("TimeLeft = RestTime")
+                        cycle -= 1
+                        print("Cycle = \(cycle)")
+                        timerState.toggle()
+                        print("TimeState = \(timerState)")
+                    }
+                }
+            }
+            timeLeft -= 1
+            print("TimeLefe = \(timeLeft)")
+            if (cycle == 0) && (timeLeft == 0) {
+                t.invalidate()
+                print("Timer has Invalidate!")
+            }
+        })
     }
 }
--- FillTimeTests/TestTimeClass.swift
+++ FillTimeTests/TestTimeClass.swift
@@ -20,44 +20,18 @@ final class TestTimeClass: XCTestCase {
         timerTests = nil
     }
     
-    // MARK: 10초동안 동작하는 타이머
+    // MARK: 5초동안 동작하는 타이머
     func testStartTimer() {
-        timerTests.startTimer(for: 10)
-        XCTAssertTrue(timerTests.timer.isValid, "It's Still Running!") 
-    }
-
-    // MARK: 타이머가 5초 뒤에 멈춘다
-    func testTimerWillStopIn5sec() throws {
-        var timeLeft = 5
-        let expectation = self.expectation(description: "Timer completes")
-
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
-            if timeLeft == 0 {
-                print(timeLeft)
-                timer.invalidate()
-                // 비동기 작업이 완료되었음을 알림
-                expectation.fulfill()
-            } else {
-                print(timeLeft)
-                timeLeft -= 1
-            }
-        }
-        // 비동기 작업이 6초 이상 소요되면 에러 출력
-        waitForExpectations(timeout: 6) { error in
-            if let error = error {
-                XCTFail("Timer did not finish in time: \(error)")
-            }
-        }
+        timerTests.startTimer()
+        XCTAssertTrue(timerTests.timer.isValid, "It's Still Running!")
     }
     
-    func testGetAfter1MinuteDate() throws {
-        var currentDate = Date.now
-        var secondData = 60.0
-        var afterDate: Date?
-        
-        afterDate = currentDate.addingTimeInterval(secondData)
-        print(currentDate)
-        print(afterDate!)
+    func testStartLoopTimer() {
+        timerTests.workTime = 5
+        timerTests.restTime = 5
+        timerTests.cycle = 2
+        timerTests.startLoopTimer()
+        XCTAssertTrue(timerTests.timer.isValid, "It's Still Running!")
     }
 
     func testPerformanceExample() throws {
