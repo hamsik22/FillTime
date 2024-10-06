6f5826816efc58eebf032322c11c76ed2bceac84
HwangSukHyeon <payback9488@gmail.com>
2024-10-06 22:40:37 +0900

    주석 추가

--- FillTime/Model/TimeModel.swift
+++ FillTime/Model/TimeModel.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 class TimeModel: ObservableObject {
+    // 여러 타이머를 다루기 위해서 필요한 배열
     @Published var contentList: [TimeData] = []
 }
 
@@ -18,6 +19,7 @@ class TimeModel: ObservableObject {
 /// - createdAt: Date = 자동 초기화
 /// - title: String = 사용자 입력값
 /// - totalSeconds: Int = 0으로 초기화
+/// - timer: Timer = 각 타이머마다 타이머를 개별적으로 관리하기 위해 자동 초기화
 class TimeData: Identifiable {
     let id = UUID()
     let createdAt = Date.now
